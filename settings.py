f0f8f6f9a1e2418a575b854903e7af0098099eb1
Anthony Scalisi <scalisi.a@gmail.com>
2013-05-06 14:41:45 -0700

    basic configuration file

---
+++ settings.py
@@ -0,0 +1,15 @@
+# AWS credentials
+
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+SSH_KEY = 'id_rsa-aws'
+IDENTITY_FILE = '~/.ssh/id_rsa-aws'
+
+# EC2 settings
+
+NODE_NAME = 'chefabulous'
+REGION = 'us-west-1'
+AVAILABILITY_ZONE = 'us-west-1a'
+IMAGE = 'ami-2928076c'
+FLAVOR = 't1.micro'
+SSH_USER = 'ubuntu'
